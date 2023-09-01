d343f65aa16490b841b9e7c9fffbe294bcd78638
Dmitry Kovalenko <mihalk9292@gmail.com>
2023-09-01 15:57:41 +0300

    init: start task towel-sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  const newArr = []
+  const result = []
+  if (matrix === undefined) {
+    return []
+  }
+matrix.forEach(element => {
+   if (matrix.indexOf(element) % 2 !== 0) {
+      newArr.push(element.reverse())
+    } else {
+      return newArr.push(element)
+    }
+  })
+newArr.forEach(element => {
+  element.forEach(el => {
+    result.push(el)
+  })
+})
+return result;
 }
